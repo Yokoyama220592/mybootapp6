8b43e6ba0302f6d378d1b31729645d43c44897a5
Your Name <you@example.com>
2025-06-11 22:35:07 +0900

    kadai9

---
+++ src/test/java/jp/te4a/spring/boot/myapp9/mybootapp9/BookApplicationTest.java
@@ -0,0 +1,13 @@
+package jp.te4a.spring.boot.myapp9.mybootapp9;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class BookApplicationTest {
+ public static void main(String[] args) {
+ SpringApplication.run(BookApplicationTest.class, args);
+ }
+}
