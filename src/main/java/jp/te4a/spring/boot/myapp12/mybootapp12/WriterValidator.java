44c9e4f6b201c11cde9d9627541715effbe501fc
Your Name <you@example.com>
2025-06-12 15:40:27 +0900

    kadai11

---
+++ src/main/java/jp/te4a/spring/boot/myapp12/mybootapp12/WriterValidator.java
@@ -0,0 +1,31 @@
+package jp.te4a.spring.boot.myapp12.mybootapp12;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class WriterValidator implements ConstraintValidator<WriterValid,String> {
+    String ok;
+    @Override
+    public void initialize(WriterValid nv){ok = nv.ok();}
+    @Override
+    /*public boolean isValid(String in,ConstraintValidatorContext cxt){
+        if(in == null){
+            return false;
+        }
+        System.out.println(in.equals(ok));
+        return in.equals(ok);
+    }*/
+    public boolean isValid(String in,ConstraintValidatorContext cxt){
+        if (in == null) return true;
+ 
+        if (in.equals(ok)) {
+            return true;
+        } else {
+            cxt.disableDefaultConstraintViolation();
+            cxt.buildConstraintViolationWithTemplate("Input " + in)
+                    .addConstraintViolation();
+            return false;
+        }
+    }
+    
+}
